056374f8c68e3db9c2a4a2f12cbd06686f808d5e
Sean Malone <seanmalone1993@gmail.com>
2022-04-08 20:42:26 +0100

    Add object with operator functions

--- calculator.js
+++ calculator.js
@@ -1,31 +1,19 @@
-function addition(num1, num2) {
-if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'ERROR, please enter number'
-} else {
- return num1 + num2;
-}
+
+let operations = {
+   '+' : function (num1, num2) {
+      return num1 + num2;
+   },
+   '-' : function(num1, num2){
+      return num1 - num2;
+   },
+   '*' : function(num1, num2) {
+      return num1 * num2;
+   },
+   '/' : function(num1, num2){
+      return num1 / num2;
+   }
 };
 
-function subtraction(num1, num2) {
- if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'ERROR, please enter number'
- } else {
-  return num1 - num2;
-} 
-};
-
-function multiply(num1, num2) {
- if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-     return 'ERROR, please enter number'
-  } else {
-   return num1 * num2;
- }    
-};
-
-function divide(num1, num2){
- if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-     return 'ERROR, please enter number'
-  } else {
-   return num1 / num2;
-}       
+function operate(list, operator) {
+   return list.reduce(operations[operator]);
 };
